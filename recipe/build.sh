626a7a557d2fa3728c18237605fd2371bbf786b5
Uwe Korn <uwelk@xhochy.com>
2024-06-26 09:19:02 +0000

    Don't set optimization as part of flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,11 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
 
+if [[ "${target_platform}" == linux-* ]]; then
+  export CFLAGS=${CFLAGS//-O2/}
+  export CXXFLAGS=${CXXFLAGS//-O2/}
+fi
+
 ./configure --prefix=$PREFIX
 
 make -j$CPU_COUNT
